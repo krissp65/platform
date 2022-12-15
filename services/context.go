cb2319a80d8c9035f36650bf4e1dd755a7f261ba
Chris Pieszkur <christof.pieszkur@mercedes-benz.com>
2022-12-15 15:38:23 +0100

    initial commit

---
+++ services/context.go
@@ -0,0 +1,20 @@
+package services
+
+import (
+	"context"
+	"reflect"
+)
+
+type ServiceKeyString string
+
+const ServiceKey ServiceKeyString = "services"
+
+type serviceMap map[reflect.Type]reflect.Value
+
+func NewServiceContext(c context.Context) context.Context {
+	if c.Value(ServiceKey) == nil {
+		return context.WithValue(c, ServiceKey, make(serviceMap))
+	} else {
+		return c
+	}
+}
