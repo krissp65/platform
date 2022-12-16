dd4ed760d66b5365c777b7cb69a72283d66e61f8
Chris Pieszkur <christof.pieszkur@mercedes-benz.com>
2022-12-16 16:44:28 +0100

    add dynamic routing

--- placeholder/message_middleware.go
+++ placeholder/message_middleware.go
@@ -1,24 +1,25 @@
 package placeholder
 
 import (
-	"errors"
-	"io"
 	"platform/config"
 	"platform/pipeline"
-	"platform/services"
+	"platform/templates"
 )
 
-type SimpleMessageComponent struct{}
+type SimpleMessageComponent struct {
+	Message string
+	config.Configuration
+}
 
-func (c *SimpleMessageComponent) Init() {}
-func (c *SimpleMessageComponent) ProcessRequest(ctx *pipeline.ComponentContext, next func(*pipeline.ComponentContext)) {
-	var cfg config.Configuration
-	services.GetService(&cfg)
-	msg, ok := cfg.GetString("main:message")
-	if ok {
-		io.WriteString(ctx.ResponseWriter, msg)
+func (c *SimpleMessageComponent) ImplementsProcessRequestWithServices() {}
+func (c *SimpleMessageComponent) Init() {
+	c.Message = c.Configuration.GetStringDefault("main:message", "Default Message")
+}
+func (c *SimpleMessageComponent) ProcessRequestWithServices(ctx *pipeline.ComponentContext, next func(*pipeline.ComponentContext), executor templates.TemplateExecutor) {
+	err := executor.ExecTemplate(ctx.ResponseWriter, "simple_message.html", c.Message)
+	if err != nil {
+		ctx.Error(err)
 	} else {
-		ctx.Error(errors.New("Cannot find config setting"))
+		next(ctx)
 	}
-	next(ctx)
 }
